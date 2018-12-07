b35e6c5466ed6708cceadb19a61ed0d326f314c3
Timothy Hoang <timh1203@gmail.com>
2018-12-07 13:04:56 -0600

    Style contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 const About = () => (
   <Section>
     <Form1 action="https://formspree.io/dullesbarber@gmail.com" method="POST">
-      <ul>
+      <Ul1>
         <li>
           <input type="hidden" name="From" value="dullesbarber.com" />
         </li>
@@ -12,7 +12,7 @@ const About = () => (
           <input type="hidden" name="_gotcha" />
         </li>
         <li>
-          <p>Email us!</p>
+          <H1a>Email us!</H1a>
         </li>
         <li>
           <Input1 type="text" name="First Name" placeholder="First Name" required />
@@ -27,9 +27,9 @@ const About = () => (
           <Textarea1 type="text" name="Message" placeholder="Message" required></Textarea1>
         </li>
         <li>
-          <Input1 type="submit" value="Send" />
+          <Button1 type="submit">Send</Button1>
         </li>
-      </ul>
+      </Ul1>
     </Form1>
   </Section >
 )
@@ -42,13 +42,50 @@ const Section = styled.section`
 const Form1 = styled.form`
   margin: 0 auto;
   text-align: center;
+  width: 60%;
+  border-radius: 10px;
+  border: 2px solid white;
+  box-shadow: 0 5px 5px gray;
+`
+const H1a = styled.h1`
+  font-size: 2rem;
+  text-shadow: 1px 1px 2px gray;
+  margin: 1rem auto;
+`
+const Ul1 = styled.ul`
 `
 const Input1 = styled.input`
-  width: 200px;
-`
+  width: 50%;
+  font-size: 1.6rem;
+  padding: 1rem;
+  margin: 1rem;
+  border-radius: 2px;
+  `
 const Textarea1 = styled.textarea`
-  width: 200px;
+  width: 50%;
   height: 200px;
+  font-size: 1.6rem;
+  padding: 1rem;
+  margin: 1rem;
+  border-radius: 2px;
+`
+const Button1 = styled.button`
+  width: 20%;
+  font-size: 1.6rem;
+  padding: 1rem;
+  margin: 1rem;
+  border-radius: 2px;
+  background: #3E9F56;
+  color: white;
+  border: 2px solid black;
+  &:hover {
+    cursor: pointer;
+    background: white;
+    color: black;
+    transform: scale(1.10);
+    border-radius: 5px;
+    border: 2px solid #3E9F56;
+  }
 `
 
 export default About
