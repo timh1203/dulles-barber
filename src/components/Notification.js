36676b6312d0c6a5af951fc15c8fe2d7e5ebef9d
Timothy Hoang <timh1203@gmail.com>
2019-05-24 08:59:25 -0500

    Change to yarn lock and add memorial day notification

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
-const Notification = (props) => (
-  <Div1>
-    <P1>
-      Ready for the upcoming season? Look for our savings ad in Valpak!
-      {/* <br />Happy Holidays! &nbsp;
-      <span role="img" aria-label="icon">🎉</span>
-      <span role="img" aria-label="icon">🐰</span>
-      <span role="img" aria-label="icon">🥚</span> */}
-    </P1>
-  </Div1>
-)
+const Notification = props => (
+	<Div1>
+		<P1>
+			We will be closed on Memorial Day (Monday the 27th)! Happy holidays!
+			{/* Ready for the upcoming season? Look for our savings ad in Valpak! */}
+			{/* <span role="img" aria-label="icon">🎉</span> */}
+			{/* <span role="img" aria-label="icon">🐰</span> */}
+			{/* <span role="img" aria-label="icon">🥚</span> */}
+		</P1>
+	</Div1>
+);
 
 const Div1 = styled.div`
-  background: #DAA520;
-  color: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  @media (max-width: 400px) {
-    padding: 1rem;
-  }
-`
+	background: #daa520;
+	color: white;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	@media (max-width: 400px) {
+		padding: 1rem;
+	}
+`;
 const P1 = styled.div`
-  font-size: 2rem;
-  line-height: 1.5;
-`
+	font-size: 2rem;
+	line-height: 1.5;
+`;
 
-export default Notification
+export default Notification;
