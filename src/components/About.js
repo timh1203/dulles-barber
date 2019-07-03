934ce8e1bff633e7bb955fa70c1449d630d73278
Timothy Hoang <timh1203@gmail.com>
2019-07-02 20:53:09 -0500

    Adjust image size

--- src/components/About.js
+++ src/components/About.js
@@ -70,13 +70,9 @@ const Div2 = styled.div`
   }
 `
 const Img1 = styled(Img)`
-  width: 80%;
-  height: 80%;
+  width: 70%;
   border-radius: 10px;
   box-shadow: 1px 1px 10px gray;
-  @media (max-width: 768px) {
-    height: 50%;
-  }
 `
 const Hr = styled.hr`
   width: 20%;
