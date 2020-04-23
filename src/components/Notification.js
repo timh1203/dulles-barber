0c593748872423ceb4c0e7082f093bc1215cc585
Timothy H <timh1203@gmail.com>
2020-04-22 21:27:42 -0500

    Update covid19 reopen date msg

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -5,16 +5,15 @@ const Notification = props => (
   <Div1>
     <P1>
       <Span1>Closed Temporarily</Span1> <br />
-      Due to the current situation, we have closed our Ashburn and Sterling locations until further notice. <br />
+      We have not decided on a re-opening date just yet, we're working towards this as policies develop. <br />
       At this time, we are not doing house calls or taking appointments. We are sorry for this inconvenience. <br />
-      Also, we greatly appreciate the influx of support from our customers but not accepting donations/gift cards purchases. <br />
-      We are managing and aware of the government programs. We are looking brightly to the future and for now, <br />
-      we are following mandated procedures until June. Please stay safe and we will make new updates right here on our website first. <br />
+      We greatly appreciate the influx of support but not accepting donations/gift cards purchases. <br />
+      Please stay safe and we will make new updates right here on our website first. <br />
       We thank you for your understanding in the interest of your health and the general public. <br />
       {/* <span role="img" aria-label="icon">🎉</span> */}
       {/* <span role="img" aria-label="icon">🐰</span> */}
       {/* <span role="img" aria-label="icon">🥚</span> */}
-      <Span2>Last updated: April 2nd, 2020</Span2>
+      <Span2>Last updated: April 22nd, 2020</Span2>
     </P1>
   </Div1>
 );
@@ -34,6 +33,7 @@ const P1 = styled.p`
   line-height: 1.5;
   color: black;
   margin: 10px 0;
+  width: 80%;
 `;
 const Span1 = styled.span`
   color: red;
